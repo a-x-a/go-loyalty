c6320c05c72358903f1a66af9f06f357f0e50a34
axa <a.x.a@inbox.ru>
2024-05-18 01:52:50 +1100

    ADD: services interfaces

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,7 +1,34 @@
 package service
 
-type Service struct {
-}
+import (
+	"context"
+
+	"github.com/a-x-a/go-loyalty/internal/model"
+)
+
+type (
+	Service struct {
+		UserService
+		OrderService
+		BallanceService
+	}
+
+	UserService interface {
+		RegisterUser(ctx context.Context, login, password string) (string, error)
+		Login(ctx context.Context, login, password string) (string, error)
+	}
+
+	OrderService interface {
+		UploadOrder(ctx context.Context, userID int64, number string) error
+		GetAllOrders(ctx context.Context, userID int64) (*model.Orders, error)
+	}
+
+	BallanceService interface {
+		GetBalance(ctx context.Context, userID int64) (*model.Balance, error)
+		WithdrawBalance(ctx context.Context, userID int64, number string, sum float64) error
+		GetWithdrawalsBalance(ctx context.Context, userID int64) (*model.Withdrawals, error)
+	}
+)
 
 func New() *Service {
 	return &Service{}
