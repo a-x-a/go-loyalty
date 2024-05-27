5ab382f92426e0ead2eb5f7b66f3eb3d578a9d88
axa <a.x.a@inbox.ru>
2024-05-28 09:15:21 +1100

    add table test for order, luhn

--- internal/model/order_test.go
+++ internal/model/order_test.go
@@ -7,15 +7,19 @@ import (
 )
 
 func TestOrderStatus_String(t *testing.T) {
-	assert.Equal(t, "NEW", NEW.String())
-	assert.Equal(t, "PROCESSING", PROCESSING.String())
-	assert.Equal(t, "INVALID", INVALID.String())
-	assert.Equal(t, "PROCESSED", PROCESSED.String())
+	assert := assert.New(t)
+
+	assert.Equal("NEW", NEW.String())
+	assert.Equal("PROCESSING", PROCESSING.String())
+	assert.Equal("INVALID", INVALID.String())
+	assert.Equal("PROCESSED", PROCESSED.String())
 }
 
 func TestOrderStatus_Index(t *testing.T) {
-	assert.Equal(t, 1, NEW.Index())
-	assert.Equal(t, 2, PROCESSING.Index())
-	assert.Equal(t, 3, INVALID.Index())
-	assert.Equal(t, 4, PROCESSED.Index())
+	assert := assert.New(t)
+
+	assert.Equal(1, NEW.Index())
+	assert.Equal(2, PROCESSING.Index())
+	assert.Equal(3, INVALID.Index())
+	assert.Equal(4, PROCESSED.Index())
 }
--- pkg/luhn/luhn_test.go
+++ pkg/luhn/luhn_test.go
@@ -1,40 +1,45 @@
 package luhn
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func Test_Check(t *testing.T) {
-    tests := []struct {
-        input string
-        want  bool
-    }{
-        {"5062821234567892", true},
-        {"5062821734567892", false},
-        {"123456789012345678901234567891", true},
-        {"123456789012345678901234567890", false},
-    }
+	assert := assert.New(t)
 
-    for _, tt := range tests {
-        got := Check(tt.input)
-        if got != tt.want {
-            t.Errorf("Check(%q) = %v, want %v", tt.input, got, tt.want)
-        }
-    }
+	tests := []struct {
+		input string
+		want  bool
+	}{
+		{"5062821234567892", true},
+		{"5062821734567892", false},
+		{"5062 8212 3456 7892", false},
+		{"123456789012345678901234567891", true},
+		{"123456789012345678901234567890", false},
+	}
+
+	for _, tt := range tests {
+		got := Check(tt.input)
+		assert.Equal(tt.want, got)
+	}
 }
 
 func Test_revers(t *testing.T) {
-    tests := []struct {
-        input string
-        want  string
-    }{
-        {"1234567890", "0987654321"},
-        {"gopher", "rehpog"},
-        {"", ""},
-    }
+	assert := assert.New(t)
 
-    for _, tt := range tests {
-        got := revers(tt.input)
-        if got != tt.want {
-            t.Errorf("revers(%q) = %q, want %q", tt.input, got, tt.want)
-        }
-    }
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{"1234567890", "0987654321"},
+		{"gopher", "rehpog"},
+		{"", ""},
+	}
+
+	for _, tt := range tests {
+		got := revers(tt.input)
+		assert.Equal(tt.want, got)
+	}
 }
